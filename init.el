b811a8544a53c2116a6add172b91af50ce006821
kawamura <kawamura@jsk.imi.i.u-tokyo.ac.jp>
2015-12-16 16:04:13 +0900

    Edit white space mode

--- init.el
+++ init.el
@@ -1,7 +1,8 @@
 (global-set-key "\C-h" 'delete-backward-char)
 (global-set-key "\M-g" 'goto-line)
 
-(set-background-color "#110011")
+(defvar my/bg-color "#111100")
+(set-background-color my/bg-color)
 (set-foreground-color "#EEFFFF")
 
 (setq inhibit-startup-message t)
@@ -27,19 +28,54 @@
 
 (setq x-select-enable-clipboard t)
 
-;;
-;; whitespace
-;;
+;; frame size
+(if (boundp 'window-system)
+    (setq default-frame-alist
+      (append (list
+	   '(width  . 70)
+	   '(height .  40)
+	   )
+	  default-frame-alist)))
+(setq initial-frame-alist default-frame-alist)
+
+;;white space settings
 (require 'whitespace)
 (setq whitespace-style '(face           ; faceで可視化
-                         trailing       ; 行末
-                         tabs           ; タブ
-                         empty          ; 先頭/末尾の空行
-                         space-mark     ; 表示のマッピング
-                         tab-mark
-                         ))
-
+			 trailing       ; 行末
+			 tabs           ; タブ
+			 spaces         ; スペース
+			 empty          ; 先頭/末尾の空行
+			 space-mark     ; 表示のマッピング
+			 tab-mark
+			 ))
 (setq whitespace-display-mappings
-      '((tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
+      '((space-mark ?\u3000 [?\u25a1])
+	;; WARNING: the mapping below has a problem.
+	;; When a TAB occupies exactly one column, it will display the
+	;; character ?\xBB at that column followed by a TAB which goes to
+	;; the next TAB column.
+	;; If this is a problem for you, please, comment the line below.
+	(tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
+
+;; スペースは全角のみを可視化
+(setq whitespace-space-regexp "\\(\u3000+\\)")
+
+;; 保存前に自動でクリーンアップ
+(setq whitespace-action '(auto-cleanup))
 
 (global-whitespace-mode 1)
+
+(set-face-attribute 'whitespace-trailing nil
+		    :background my/bg-color
+		    :foreground "DeepPink"
+		    :underline t)
+(set-face-attribute 'whitespace-tab nil
+		    :background my/bg-color
+		    :foreground "LightSkyBlue"
+		    :underline t)
+(set-face-attribute 'whitespace-space nil
+		    :background my/bg-color
+		    :foreground "GreenYellow"
+		    :weight 'bold)
+(set-face-attribute 'whitespace-empty nil
+		    :background my/bg-color)
